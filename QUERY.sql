c9256a4fb8a658ed752000497df9fa44f17c8e44
NahinKh <nahinkhan77@gmail.com>
2026-07-08 01:03:13 +0600

    booking details with user and match information

--- QUERY.sql
+++ QUERY.sql
@@ -67,3 +67,15 @@ SELECT booking_id,
        AS systematic_status
 FROM Bookings
 WHERE payment_status IS NULL;
+
+-- =========================================================================
+--Query 4: Booking details with user and match information
+-- =========================================================================
+SELECT b.booking_id,
+       u.full_name,
+       m.fixture,
+       b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id
+
